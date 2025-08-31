292aa1003b7ab9bc65c74b4771289cfee1ad0987
itsmebharat84 <itsme.bharat84@gmail.com>
2025-08-31 23:28:32 +0530

    add 4000 port

--- main.tf
+++ main.tf
@@ -31,47 +31,6 @@ data "aws_subnets" "default_public" {
   }
 }
 
-# Security Group: SSH open to the world (IPv4 & IPv6)
-resource "aws_security_group" "ec2_sg" {
-  name        = "tfe-ec2-ssh-anywhere"
-  description = "Allow SSH from anywhere"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    description = "SSH IPv4"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.ssh_cidr]
-  }
-
-  ingress {
-    description      = "SSH IPv6"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    description = "All egress"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name        = "ec2-ssh-anywhere"
-    Project     = var.project
-    Owner       = var.owner
-    CostCenter  = var.cost_center
-    ManagedBy   = "terraform"
-    Environment = "dev"
-  }
-}
-
 # Option A: generate a fresh SSH key pair
 resource "tls_private_key" "ssh" {
   algorithm = "RSA"
--- sg.tf
+++ sg.tf
@@ -3,13 +3,24 @@ resource "aws_security_group" "ec2_sg" {
   description = "Allow SSH and app access"
   vpc_id      = data.aws_vpc.default.id
 
-  # SSH
-  ingress {
+ 
+
+    ingress {
+    description = "SSH IPv4"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.ssh_cidr]
   }
+
+  ingress {
+    description      = "SSH IPv6"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 
   # Port 4000 (open to the world)
   ingress {
@@ -19,14 +30,23 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
+
+
+    egress {
+    description = "All egress"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
-    Name = "ec2-sg"
+    Name        = "ec2-ssh-anywhere"
+    Project     = var.project
+    Owner       = var.owner
+    CostCenter  = var.cost_center
+    ManagedBy   = "terraform"
+    Environment = "dev"
   }
 }
