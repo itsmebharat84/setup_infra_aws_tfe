df68bb2b2b9bafb23efc5f5fe54aa857bfa894e4
itsmebharat84 <itsme.bharat84@gmail.com>
2025-08-31 23:07:16 +0530

    provision tfe instance

--- outputs.tf
+++ outputs.tf
@@ -12,8 +12,6 @@ output "ssh_private_key_path" {
 }
 
 output "ssh_example_command" {
-  value = var.use_existing_key_name ?
-    format("ssh -i ~/.ssh/%s.pem ec2-user@%s", var.existing_key_name, aws_instance.dev_box.public_dns) :
-    format("ssh -i %s ec2-user@%s", local_file.private_key_pem[0].filename, aws_instance.dev_box.public_dns)
+  value = var.use_existing_key_name ? format("ssh -i ~/.ssh/%s.pem ec2-user@%s", var.existing_key_name, aws_instance.dev_box.public_dns) : format("ssh -i %s ec2-user@%s", local_file.private_key_pem[0].filename, aws_instance.dev_box.public_dns)
 }
 
